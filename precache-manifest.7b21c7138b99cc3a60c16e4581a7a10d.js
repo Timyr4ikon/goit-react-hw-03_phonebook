16b10027929071ff099d50912fdf1578864cb1ae
Timyr4ikon <54175850+Timyr4ikon@users.noreply.github.com>
2020-06-21 03:23:18 +0300

    Updates

---
+++ precache-manifest.7b21c7138b99cc3a60c16e4581a7a10d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd9f797b69161877272177403f094106",
+    "url": "/goit-react-hw-03_phonebook/index.html"
+  },
+  {
+    "revision": "c8e978813df00c81285b",
+    "url": "/goit-react-hw-03_phonebook/static/css/2.f884ae05.chunk.css"
+  },
+  {
+    "revision": "db91d1941ee7ba011258",
+    "url": "/goit-react-hw-03_phonebook/static/css/main.4f8242b2.chunk.css"
+  },
+  {
+    "revision": "c8e978813df00c81285b",
+    "url": "/goit-react-hw-03_phonebook/static/js/2.012524ee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/goit-react-hw-03_phonebook/static/js/2.012524ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db91d1941ee7ba011258",
+    "url": "/goit-react-hw-03_phonebook/static/js/main.2966c008.chunk.js"
+  },
+  {
+    "revision": "2bfcb69b916a877dece0",
+    "url": "/goit-react-hw-03_phonebook/static/js/runtime-main.65a6bc53.js"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/goit-react-hw-03_phonebook/static/media/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/goit-react-hw-03_phonebook/static/media/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/goit-react-hw-03_phonebook/static/media/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "d120c85b6eb0549d60483e2980033c15",
+    "url": "/goit-react-hw-03_phonebook/static/media/MaterialIcons-Regular.d120c85b.ttf"
+  }
+]);
